d11fc7faa4a7045ff604a27aafbf044e486585af
Shahzaib Waseem <msw10@sfu.ca>
2023-07-19 15:27:14 -0700

    correcting reconstruction file.

--- app/src/main/java/com/shahzaib/mobispectral/Reconstruction.kt
+++ app/src/main/java/com/shahzaib/mobispectral/Reconstruction.kt
@@ -24,25 +24,34 @@ class Reconstruction(context: Context, modelPath: String) {
         return TensorImageUtils.bitmapToFloat32Tensor(bitmap, mean, std)
     }
 
-    private fun getOneBand(tensor: Tensor): Tensor {
+    private fun processTensor(floatArray: FloatArray, numberOfElements: Int, min: Float, max: Float, channels: Long): Tensor {
+        val diff = max - min
+        for (i in 0 until numberOfElements) {
+            floatArray[i] = (floatArray[i] - min)/diff
+        }
+        val size = longArrayOf(1, channels, bitmapsHeight.toLong(), bitmapsWidth.toLong())
+        return Tensor.fromBlob(floatArray, size)
+    }
+    private fun getOneBand(tensor: Tensor, offset: Int): Tensor {
         val tensorDoubleArray = tensor.dataAsFloatArray
         val floatArray = FloatArray((bitmapsHeight*bitmapsWidth))
+        val bandOffset = bitmapsHeight*bitmapsWidth*offset
         Log.i("Tensor size", "${tensorDoubleArray.size}")
         for (i in 0 until (bitmapsHeight*bitmapsWidth)){
-            floatArray[i] = tensorDoubleArray[i]
+            floatArray[i] = tensorDoubleArray[bandOffset+i]
         }
         val size = longArrayOf(1, 1, bitmapsHeight.toLong(), bitmapsWidth.toLong())
         return Tensor.fromBlob(floatArray, size)
     }
 
-    private fun concatenate(rgbTensor: Tensor, nirTensor: Tensor): Tensor {
-        val rgbArray = rgbTensor.dataAsFloatArray
-        val nirArray = nirTensor.dataAsFloatArray
+    private fun concatenate(tensor1: Tensor, tensor2: Tensor, channels: Long): Tensor {
+        val rgbArray = tensor1.dataAsFloatArray
+        val nirArray = tensor2.dataAsFloatArray
         val concatenated = FloatArray(rgbArray.size + nirArray.size)
         System.arraycopy(rgbArray, 0, concatenated, 0, rgbArray.size)
         System.arraycopy(nirArray, 0, concatenated, rgbArray.size, nirArray.size)
         Log.i("Concatenated Array Size", "${concatenated.size}")
-        val size = longArrayOf(1, 4, bitmapsHeight.toLong(), bitmapsWidth.toLong())
+        val size = longArrayOf(1, channels, bitmapsHeight.toLong(), bitmapsWidth.toLong())
         return Tensor.fromBlob(concatenated, size)
     }
 
@@ -50,11 +59,19 @@ class Reconstruction(context: Context, modelPath: String) {
         bitmapsWidth = rgbBitmap.width
         bitmapsHeight = rgbBitmap.height
 
-        val rgbTensor: Tensor = preprocess(rgbBitmap)
-        val nirTensor: Tensor = getOneBand(preprocess(nirBitmap))
-        Log.i("TensorShape", "${rgbTensor.shape().toList()}, ${nirTensor.shape().toList()}")
+        val rgbBitmapTensor = preprocess(rgbBitmap)
+        val redBand: Tensor = getOneBand(rgbBitmapTensor, 0)
+        val greenBand: Tensor = getOneBand(rgbBitmapTensor, 1)
+        val blueBand: Tensor = getOneBand(rgbBitmapTensor, 2)
+        var bgrTensor = concatenate(blueBand, greenBand, 2)
+        bgrTensor = concatenate(bgrTensor, redBand, 3)
+        bgrTensor = processTensor(bgrTensor.dataAsFloatArray, bitmapsHeight*bitmapsWidth*3, bgrTensor.dataAsFloatArray.min(), bgrTensor.dataAsFloatArray.max(), 3)
 
-        val imageTensor: Tensor = concatenate(rgbTensor, nirTensor)
+        var nirTensor: Tensor = getOneBand(preprocess(nirBitmap), 0)
+        nirTensor = processTensor(nirTensor.dataAsFloatArray, bitmapsHeight*bitmapsWidth, nirTensor.dataAsFloatArray.min(), nirTensor.dataAsFloatArray.max(), 1)
+        Log.i("TensorShape", "${bgrTensor.shape().toList()}, ${nirTensor.shape().toList()}")
+
+        val imageTensor: Tensor = concatenate(bgrTensor, nirTensor, 4)
 
         Log.i("Concatenated", imageTensor.shape().toList().toString())
 
